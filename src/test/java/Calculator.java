c48c80b8bfb4a5dfd5e1e8062f1572714cfec0d3
RuslanBaltabayev <49660092+RuslanBaltabayev@users.noreply.github.com>
2019-07-31 20:49:31 -0700

    delete unnecessary things

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -3,11 +3,9 @@ public class Calculator {
     public static double add(double num1,double num2){
         return num1+num2;
     }
-<<<<<<< HEAD
    public static double subtract(double num1,double num2){
         return num1-num2;
    }
-=======
     public static void main(String[] args) {
 
     }
@@ -15,5 +13,4 @@ public class Calculator {
     public static double multiply(double num1,double num2){
         return num1*num2;
     }
->>>>>>> e1b9d007455a89f3231922cafb6c0594e6884466
 }
